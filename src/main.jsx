939af13942bb79747568d94db90906a6bc2ee5d4
unknown <mr.alan.tran@gmail.com>
2024-11-17 21:48:26 -0800

    revert browser router to old

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import enigmatic from "./assets/images/enigmatic.png";
 import ricecake from "./assets/images/ricecake.png";
 import yots from "./assets/images/yots.png";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      errorElement: <div>error</div>,
-    },
-  ],
-  { basename: import.meta.env.DEV ? "/" : "/sdtet24/" }
-);
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} fallbackElement={<div>loading...</div>} />
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/sdtet24/"}>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
