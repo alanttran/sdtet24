b262c205a2543b6ffbb341167aff522f1a9b70ff
unknown <mr.alan.tran@gmail.com>
2024-11-17 20:41:05 -0800

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
-import {
-  BrowserRouter,
-  RouterProvider,
-} from "react-router-dom"
-import enigmatic from './assets/images/enigmatic.png'
-import ricecake from './assets/images/ricecake.png'
-import yots from './assets/images/yots.png'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import enigmatic from "./assets/images/enigmatic.png";
+import ricecake from "./assets/images/ricecake.png";
+import yots from "./assets/images/yots.png";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>error</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
-
-
-
-
+    <RouterProvider router={router} fallbackElement={<div>loading...</div>} />
+  </StrictMode>
+);
