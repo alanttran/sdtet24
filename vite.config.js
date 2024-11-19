ae263e7dceebf459c2bb87097cf00dbfd1a177c0
unknown <mr.alan.tran@gmail.com>
2024-11-18 23:54:56 -0800

    removed 404

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,6 @@ export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
     base: "/",
-    build: {
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, "index.html"),
-          404: resolve(__dirname, "public/404.html"),
-        },
-      },
-    },
   };
 
   if (command !== "serve") {
